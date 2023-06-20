ddfd28faebe5a5d6a9f3a4244ff642f48cce4653
JARS3N <JARS3N@users.noreply.github.com>
2023-06-20 15:02:46 -0400

    Update server.R

--- R/server.R
+++ R/server.R
@@ -5,13 +5,14 @@ server <- function() {
   function(input, output, session) {
     observeEvent(input$upload,
                  {
-                   atm::edit_atm(input$upload$datapath, input$num)
+                  
                    print(input$upload$name)
                    output$dlfile <- downloadHandler(
                      filename = function() {
                        input$upload$name
                      },
                      content = function(file) {
+                       atm::edit_atm(input$upload$datapath, input$num)
                        file.copy(input$upload$datapath, file)
                      }
 
